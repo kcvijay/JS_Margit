e9dc8e6e474ee0daba5d1f484957e5ecb578a212
KC Vijay <s2200734@BC-C02J61CDQ6LR-MacBook-Air.local>
2022-09-06 15:26:44 +0300

    Bug fixes, new content added

--- Weekly Exercises/Week_5/vehicle_registration_table/main.js
+++ Weekly Exercises/Week_5/vehicle_registration_table/main.js
@@ -2,94 +2,125 @@ const dataInput = document.querySelectorAll(".data-input");
 const createData = document.getElementById("submit");
 const table = document.querySelector("tbody");
 const form = document.querySelector("form");
-const registrationInput = document.getElementById("reg-no");
+const registrationInput = document.getElementById("reg-year");
 
-let searchResult = document.getElementById("search-result-txt");
+const searchBox = document.getElementById("search");
+const searchBtn = document.getElementById("btn-search");
+let searchResultTxt = document.getElementById("search-result-txt");
+
+let tableDataRows = [];
 
 /**************/
 
 function createVehicle(e) {
-    let owner = document.getElementById("owner-name").value.toUpperCase();
-    let registration = document.getElementById("reg-no").value.toUpperCase();
-    let company = document.getElementById("vehicle-company").value.toUpperCase();
-    let type =  document.getElementById("vehicle-type").value.toUpperCase();
-    let model =  document.getElementById("vehicle-model").value.toUpperCase();
-    let year =  document.getElementById("reg-year").value;
+  // Creation of dynamic input data every time
+  let owner = document.getElementById("owner-name").value.toUpperCase();
+  let registration = document.getElementById("reg-no").value.toUpperCase();
+  let company = document.getElementById("vehicle-company").value.toUpperCase();
+  let type = document.getElementById("vehicle-type").value.toUpperCase();
+  let model = document.getElementById("vehicle-model").value.toUpperCase();
+  let year = document.getElementById("reg-year").value;
 
-    if(!owner || !registration || !company || !type || !model || !year) {
-        e.preventDefault();
-        return false;
-    }
-   
-    function vehicle(owner, registration, company, type, model, year) { // Constuctor
-        this.owner = owner;
-        this.registration = registration;
-        this.company = company;
-        this.type = type;
-        this.model = model;
-        this.year = year;
-    }
-    
-    let newVehicle = new vehicle(owner, registration, company, type, model, year);
-    console.log(newVehicle);
-    addTableRow(owner, registration, company, type, model, year);
-    form.reset();
+  if (!owner || !registration || !company || !type || !model || !year) {
+    e.preventDefault();
+    return false;
+  }
+
+  function vehicle(registration, owner, company, model, type, year) {
+    this.registration = registration;
+    this.owner = owner;
+    this.company = company;
+    this.model = model;
+    this.type = type;
+    this.year = year;
+  }
+
+  let newVehicle = new vehicle(registration, owner, company, model, type, year);
+  tableDataRows.push(newVehicle);
+  addTableRow(registration, owner, company, model, type, year);
+  form.reset();
+  console.log(tableDataRows);
 }
 
-createData.addEventListener("click", createVehicle); // on both clicking the submit and entering from the last input
-registrationInput.addEventListener("keyup", (event)=> {
-    if(event.key === "Enter") {
-        createVehicle();
-        addTableRow(owner, registration, company, type, model, year);
-        form.reset();
-    }
-})
+createData.addEventListener("click", createVehicle); // Creating a table data row on both clicking the submit and pressing Enter on last input box.
+registrationInput.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    createVehicle();
+  }
+});
 
-function addTableRow(cell1value, cell2value, cell3value, cell4value, cell5value, cell6value) {
-    const tableRow = document.createElement("tr");
-    const cell1 = document.createElement("td");
-    const cell2 = document.createElement("td");
-    const cell3 = document.createElement("td");
-    const cell4 = document.createElement("td");
-    const cell5 = document.createElement("td");
-    const cell6 = document.createElement("td");
+function addTableRow( // Function for adding table row and contents.
+  cell1value,
+  cell2value,
+  cell3value,
+  cell4value,
+  cell5value,
+  cell6value
+) {
+  const tableRow = document.createElement("tr");
+  const cell1 = document.createElement("td");
+  const cell2 = document.createElement("td");
+  const cell3 = document.createElement("td");
+  const cell4 = document.createElement("td");
+  const cell5 = document.createElement("td");
+  const cell6 = document.createElement("td");
 
-    table.appendChild(tableRow);
-    tableRow.appendChild(cell1);
-    tableRow.appendChild(cell2);
-    tableRow.appendChild(cell3);
-    tableRow.appendChild(cell4);
-    tableRow.appendChild(cell5);
-    tableRow.appendChild(cell6);
+  table.appendChild(tableRow);
+  tableRow.appendChild(cell1);
+  tableRow.appendChild(cell2);
+  tableRow.appendChild(cell3);
+  tableRow.appendChild(cell4);
+  tableRow.appendChild(cell5);
+  tableRow.appendChild(cell6);
 
-    cell1.textContent = cell1value;
-    cell2.textContent = cell2value;
-    cell3.textContent = cell3value;
-    cell4.textContent = cell4value;
-    cell5.textContent = cell5value;
-    cell6.textContent = cell6value;
+  cell1.textContent = cell1value;
+  cell2.textContent = cell2value;
+  cell3.textContent = cell3value;
+  cell4.textContent = cell4value;
+  cell5.textContent = cell5value;
+  cell6.textContent = cell6value;
 }
 
-let input = document.getElementById("search");
-function searchItem() {
-    let myTable, tr, td, textValue;
-    filterTxt = input.value.toUpperCase();
-    myTable = document.getElementById("myTable");
-    tr = myTable.querySelectorAll("tr");
+const input = document.getElementById("search--2"); // Filtering the table by vehicle owner's name /**Experimental**
+function filterItem() {
+  let myTable, tr, td, textValue;
+  filterTxt = input.value.toUpperCase();
+  myTable = document.getElementById("myTable");
+  tr = myTable.querySelectorAll("tr");
 
-    for(let i = 0; i < tr.length; i++) {
-        td = tr[i].querySelectorAll("td")[0];
-        if(td) {
-            textValue =  td.textContent;
-            if(textValue.toUpperCase().indexOf(filterTxt) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-            
-        }
+  for (let i = 0; i < tr.length; i++) {
+    td = tr[i].querySelectorAll("td")[0];
+    if (td) {
+      textValue = td.textContent;
+      if (textValue.toUpperCase().indexOf(filterTxt) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
     }
+  }
 }
 
-input.addEventListener("keyup", searchItem);
-    
+input.addEventListener("keyup", filterItem); // filtering the item in both button click and pressing Enter.
+/*************/
+searchBtn.addEventListener("click", (e) => {
+  // searching the vehicle item
+  e.preventDefault();
+  searchVehicleItem();
+});
+searchBox.addEventListener("keyup", (e) => {
+  e.preventDefault();
+  if (e.key === "Enter") {
+    searchVehicleItem();
+  }
+});
+
+function searchVehicleItem() {
+  // Function for searching vehicle data either by registration number or, owner.
+  const searchInputTxt = tableDataRows.find(function (vehicleItem) {
+    if (vehicleItem.owner == searchBox) {
+      return true;
+    }
+    searchResultTxt.textContent = `The searched vehicle is ${vehicleItem.registration}, ${vehicleItem.company} ${vehicleItem.model} (${vehicleItem.type}) registered in ${vehicleItem.year}. The owner is ${vehicleItem.owner}.`;
+  });
+}
