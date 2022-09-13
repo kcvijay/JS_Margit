4fa7a97eaf2a16daba101e8c38118930fdb5d0c7
KC Vijay <s2200734@BC-C02J61CDQ6LR-MacBook-Air.local>
2022-09-13 12:08:55 +0300

    code refactoring

--- Weekly Exercises/Week_4/bg_generator/main.js
+++ Weekly Exercises/Week_4/bg_generator/main.js
@@ -1,31 +1,17 @@
 let firstColor = document.querySelector("#first-color");
 let secondColor = document.querySelector("#second-color");
-
 const positionBtn = document.querySelectorAll("label");
 const colorTxt = document.querySelector("#colorTxt");
-
 const gradientField = document.querySelector(".main-container");
-
 const textToCopy = document.getElementById("color-code");
 
-let val1 = "#000000";
-let val2 = "#000000";
-
-let actionVal = "";
-
-firstColor.addEventListener("input", () => {
-  val1 = firstColor.value;
-  console.log(val1);
-});
-
-secondColor.addEventListener("input", () => {
-  val2 = secondColor.value;
-  console.log(val2);
-});
+let actionVal;
 
 for (let i = 0; i < positionBtn.length; i++) {
   positionBtn[i].addEventListener("click", () => {
     actionVal = positionBtn[i].id;
+    let val1 = firstColor.value;
+    let val2 = secondColor.value;
     gradientField.style.backgroundImage = `linear-gradient(${actionVal}, ${val1}, ${val2})`;
     colorTxt.innerHTML = `<p><strong>For Geeks:</strong></p>
         <p id="color-code"><strong>background-image:</strong> linear-gradient(${actionVal}, ${val1}, ${val2});</p>`;
