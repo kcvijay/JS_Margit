0b4cb41cf916709a9def2d390147b1026f859e5b
KC Vijay <s2200734@BC-C02J61CDQ6LR-MacBook-Air.local>
2022-09-13 10:07:28 +0300

    Design fixes

--- Weekly Exercises/Week_4/bg_generator/main.js
+++ Weekly Exercises/Week_4/bg_generator/main.js
@@ -1,7 +1,7 @@
 let firstColor = document.querySelector("#first-color");
 let secondColor = document.querySelector("#second-color");
 
-const positionBtn = document.querySelectorAll("label")
+const positionBtn = document.querySelectorAll("label");
 const colorTxt = document.querySelector("#colorTxt");
 
 const gradientField = document.querySelector(".main-container");
@@ -14,22 +14,20 @@ let val2 = "#000000";
 let actionVal = "";
 
 firstColor.addEventListener("input", () => {
-    val1 = firstColor.value;
-    console.log(val1);
-})
+  val1 = firstColor.value;
+  console.log(val1);
+});
 
-secondColor.addEventListener("input", ()=> {
-    val2 = secondColor.value;
-    console.log(val2);
-})
+secondColor.addEventListener("input", () => {
+  val2 = secondColor.value;
+  console.log(val2);
+});
 
-
-for(let i = 0; i < positionBtn.length; i++) {   
-    positionBtn[i].addEventListener("click", () => {
-        actionVal = positionBtn[i].id;
-        gradientField.style.backgroundImage = `linear-gradient(${actionVal}, ${val1}, ${val2})`;
-        colorTxt.innerHTML = `<p><strong>For Geeks:</strong></p>
-        <p id="color-code"><strong>background-image:</strong> linear-gradient(${actionVal}, ${val1}, ${val2});</p>`
-    })
+for (let i = 0; i < positionBtn.length; i++) {
+  positionBtn[i].addEventListener("click", () => {
+    actionVal = positionBtn[i].id;
+    gradientField.style.backgroundImage = `linear-gradient(${actionVal}, ${val1}, ${val2})`;
+    colorTxt.innerHTML = `<p><strong>For Geeks:</strong></p>
+        <p id="color-code"><strong>background-image:</strong> linear-gradient(${actionVal}, ${val1}, ${val2});</p>`;
+  });
 }
-
