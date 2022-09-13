0349108791239c96b8094e240a285335cc5a5b59
KC Vijay <s2200734@BC-C02J61CDQ6LR-MacBook-Air.local>
2022-09-13 13:29:42 +0300

    Direction button fixes

--- Weekly Exercises/Week_4/bg_generator/main.js
+++ Weekly Exercises/Week_4/bg_generator/main.js
@@ -1,20 +1,20 @@
 let firstColor = document.querySelector("#first-color");
 let secondColor = document.querySelector("#second-color");
 const positionBtn = document.querySelectorAll("label");
-const colorTxt = document.querySelector("#colorTxt");
-const gradientField = document.querySelector(".main-container");
-const textToCopy = document.getElementById("color-code");
+const codeTxt = document.querySelector("#colorTxt");
 
 let actionVal;
+let val1;
+let val2;
 
 for (let i = 0; i < positionBtn.length; i++) {
   positionBtn[i].addEventListener("click", () => {
     actionVal = positionBtn[i].id;
-    let val1 = firstColor.value;
-    let val2 = secondColor.value;
+    val1 = firstColor.value;
+    val2 = secondColor.value;
     backgroundTxt = `linear-gradient(${actionVal}, ${val1}, ${val2})`;
-    gradientField.style.backgroundImage = backgroundTxt;
-    colorTxt.innerHTML = `<p><strong>For Geeks:</strong></p>
+    document.body.style.backgroundImage = backgroundTxt;
+    codeTxt.innerHTML = `<p><strong>For Geeks:</strong></p>
         <p id="color-code"><strong>background-image:</strong>${backgroundTxt};</p>`;
   });
 }
