536d9c740653f23ef4aa90e7170aa17e82eb726c
Vijay KC <vj.kc2007@gmail.com>
2022-09-21 13:31:47 +0300

    Added new functions

--- Weekly Exercises/Week_8/speed_game/main.js
+++ Weekly Exercises/Week_8/speed_game/main.js
@@ -1,21 +1,41 @@
 const btnModalClose = document.querySelector("#modal-close");
 const btnInfo = document.querySelector(".info");
 const overlay = document.querySelector(".overlay");
-const btns = document.querySelectorAll(".btn");
+const coins = document.querySelectorAll(".coin");
+let scoreBox = document.querySelector("#score");
 
 const btnStartGame = document.querySelector("#startBtn");
 const btnStopGame = document.querySelector("#stopBtn");
+let randArray = [];
 
-const startGame = () => {
-  console.log("Game started");
+function getRandom() {
+  let rand = Math.floor(Math.random() * 4);
+  console.log(rand);
+  randArray.push(rand);
+
+  setTimeout(getRandom, 1000);
+}
+
+setTimeout(getRandom, 1000);
+
+let score = 0;
+
+coins.forEach((circle, i) => {
+  circle.addEventListener("click", () => clickCoin(i));
+});
+
+const clickCoin = (i) => {
+  score++;
+  console.log(score);
+  scoreBox.textContent = score + " " + "₿";
 };
 
-const endGame = () => {
-  console.log("Game ended");
-};
+// const endGame = () => {
+//   console.log("Game ended");
+// };
 
-btnStartGame.addEventListener("click", startGame);
-btnStopGame.addEventListener("click", endGame);
+// btnStartGame.addEventListener("click", startGame);
+// btnStopGame.addEventListener("click", endGame);
 
 const toggleModal = () => {
   overlay.classList.toggle("visible");
@@ -24,8 +44,21 @@ btnInfo.addEventListener("click", toggleModal);
 
 btnModalClose.addEventListener("click", toggleModal);
 
-for (const btn of btns) {
-  btn.addEventListener("click", () => {
-    console.log(btn.value);
-  });
-}
+// const startGame = () => {
+//   let count = 0;
+//   //Throwing random number every second.
+//   setInterval(function () {
+//     let rand = Math.floor(Math.random() * 4);
+//     console.log(rand);
+//   }, 1000);
+//   for (const coin of coins) {
+//     coin.addEventListener("click", () => {
+//       let indexOfCoin = Array.from(coin.parentNode.children).indexOf(coin);
+//       let rand = coin;
+//       console.log(indexOfCoin);
+//       count++;
+//       console.log(count);
+//       coin.classList.add("active");
+//     });
+//   }
+// };
